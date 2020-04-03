babc67f948619a076eb5a71570560057b4e68b22
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-04-03 17:18:56 -0600

    Update ProblemG.java

--- ProblemG.java
+++ ProblemG.java
@@ -1,49 +1,24 @@
 /**
-Whitespace is an esoteric programming language (esolang) created in 2003 by Edwin Brady and Chris Morris. Many elements of Whitespace are familiar, including the fact that it is imperative and stack-based, but it has one main distinguishing feature: the only significant characters in a Whitespace source code file are [Tab] (ASCII 9), [Newline] (ASCII 10), and [Space] (ASCII 32). This, of course, has the intentionally humorous result that all source code files using only these characters appear completely blank in most text editors, which can be problematic if you have a boss who keeps looking over your shoulder for evidence of productivity. (A Whitespace source code file can, in fact, include other characters, but these are simply ignored, so we will not consider that possibility here.)
+People are rarely content with what they have — they always seem to want more. Sometimes this is just healthy ambition, but at other times it is clearly motivated by shallow materialism, or worse. Perhaps we could accept this as an unavoidable aspect of human nature, but the problem is that this mindset appears to be spreading rapidly to the world of mathematics and computer science. For example, greedy algorithms are already well known for taking as much as they can in each iteration, but now geometric shapes are getting in on the act! So far, only regular polygons have been affected, but who knows where it will end?
 
-For this problem, you will work with a simplified version of Whitespace that we call Whitespace Lite, affectionately known as Litespace. Let W denote the 3-character set {[Tab], [Newline], [Space]}. A Litespace program is a sequence of instructions, each of which consists of two or more characters from W. There is a stack available that holds signed integers; when a Litespace program begins, the stack is initially empty. In the list of instructions, given below, only one instruction (the last) is specifically intended to produce output, but most other instructions produce output if an error condition is encountered.
+In a regular n-sided polygon (n-gon), all n sides are of equal length, and all interior angles are the same. (For our purposes, a polygon includes its interior, i.e., it is a solid shape.) In the past, the area of a regular n-gon with a given side length was fixed, which was very convenient for students tasked with calculating its area, but now, whenever it gets the chance, a regular n-gon will carry out an expansion operation informally known as a “land grab.” A land grab with expansion distance d>0 works as follows: every point outside the regular n-gon that is at most distance d from some point in the regular n-gon is absorbed and becomes a part of the enlarged n-gon. Over time, a regular n-gon might carry out many land grabs. (Technically, a regular n-gon ceases to be a regular n-gon after the first such land grab, but, sadly, it doesn’t notice that its greed is causing it to lose its identity.) Figure 1 depicts the two test cases in the sample input: a regular 3-gon (triangle) after a single land grab, and a regular 4-gon (square) after two land grabs.
 
-Litespace Instruction Set:
+Given an initial regular n-gon and an expansion distance d, what is the area of the resulting region after a specified number of land grabs?
 
-[Space][Space]<integer> — Push the specified integer onto the stack.
-
-[Space][Newline][Space] — Make a copy of the topmost stack element and push this copy onto the stack. If the stack is empty, print “Invalid copy operation” on a line by itself.
-
-[Space][Newline][Tab] – Swap the two topmost stack elements. If the stack contains fewer than two elements, print “Invalid swap operation” on a line by itself and leave the stack unchanged.
-
-[Space][Newline][Newline] – Remove and discard the topmost stack element. If the stack is empty, print “Invalid remove operation” on a line by itself.
-
-[Tab][Space][Space][Space] — Replace the two topmost stack elements with their sum. If the stack contains fewer than two elements, print “Invalid addition operation” on a line by itself and leave the stack unchanged.
-
-[Tab][Space][Space][Tab] — Replace the two topmost stack elements with their difference (the topmost element subtracted from the second topmost element). If the stack contains fewer than two elements, print “Invalid subtraction operation” on a line by itself and leave the stack unchanged.
-
-[Tab][Space][Space][Newline] — Replace the two topmost stack elements with their product. If the stack contains fewer than two elements, print “Invalid multiplication operation” on a line by itself and leave the stack unchanged.
-
-[Tab][Space][Tab][Space] — Replace the two topmost stack elements with their quotient (the second topmost element divided by the topmost element); this is integer division, so discard any fractional part of the quotient. If the stack contains fewer than two elements, print “Invalid division operation” on a line by itself and leave the stack unchanged. If the stack contains two or more elements, the topmost of which is 0, print “Division by zero” on a line by itself and leave the stack unchanged.
-
-[Tab][Newline][Space][Tab] — Remove the integer on the top of the stack and print it on a line by itself (in the usual base-10 representation). If the stack is empty, print “Invalid print operation” on a line by itself.
-
-In the first instruction above, an integer to be pushed onto the stack is in binary form, encoded as two or more characters from the set {[Space], [Tab]} followed by a [Newline] character. The first character encodes the sign of the integer ([Space] = positive, [Tab] = negative), and the following [Space] and [Tab] characters encode the bits in the binary representation of the absolute value (magnitude) of the integer, with the convention that [Space] =0, [Tab] =1. The first bit after the sign bit is most significant, and there are no leading zeros. For example, since 13 in binary is 1101, the encoding of −13 is
-
-[Tab][Tab][Tab][Space][Tab][Newline]
-Note that there are two encodings of 0, one for +0 and one for −0. These are interchangeable.
-
-Given a Litespace program, produce the program’s output. For compactness, the program will be given as a string of characters from the set {‘T’, ‘N’, ‘S’}, where ‘T’ denotes [Tab], ‘N’ denotes [Newline], and ‘S’ denotes [Space].
-
+\includegraphics[width=0.7\textwidth ]{shapes06}
+Figure 1: Illustration of test cases in Sample Input 1
 Input
-Input consists of a single non-empty string of characters from {‘T’, ‘N’, ‘S’} representing a valid sequence of Litespace instructions. The length of the string is at most 10000. It is guaranteed that the Litespace program produces at least one line of output. Also, any integer pushed onto the stack or generated by a successful arithmetic operation will be less than 231 in absolute value.
+The first line of input contains an integer, N, the number of test cases (1≤N≤100). Each of the next N lines contains the information for a single regular n-gon, given as four space-separated integers, n ℓ d g, where n is the initial number of sides (3≤n≤60), ℓ is the initial side length, d is the expansion distance (1≤ℓ,d≤10), and g is the number of land grabs (0≤g≤20).
 
 Output
-Print the output of the Litespace program.
+For each test case, output the area of the region occupied by the n-gon after g land grabs. Each area will be considered correct if it is within 10−6 of the official answer.
 
 Sample Input 1	Sample Output 1
-SSSTTSNTNST
-6
-Sample Input 2	Sample Output 2
-SSSTTNSSTTNSSSTTSTNTSSSTNSTTNSTTNST
-12
-3
-Invalid print operation
+2
+3 8 1 1
+4 5 2 2
+54.85440557469184
+155.2654824574367
 **/
 import java.util.Scanner;
 
